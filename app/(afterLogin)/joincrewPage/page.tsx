ac33732596209723207cdadd6f04b785b7aaaa7d
ByeongJu0411 <wjsqudwn981789@gmail.com>
2025-12-16 21:54:29 +0900

    프로필, 커뮤니티, 나의 기록 기본 세팅

---
+++ app/(afterLogin)/joincrewPage/page.tsx
@@ -0,0 +1,15 @@
+import styles from "./joincrewpage.module.css";
+
+import PageHeader from "../_component/pageheader";
+import Footer from "../_component/footer";
+export default function JoinCrewPage() {
+  return (
+    <div className={styles.container}>
+      <PageHeader mainTitle="크루 참여" subTitle="나와 맞는 크루를 찾아보고 만들어보세요 !" />
+      <div className={styles.body}>
+        <div className={styles.body_section}></div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
