d04b95192c941cf3c3b97d50914b39112ec9c9d0
ByeongJu0411 <wjsqudwn981789@gmail.com>
2025-12-17 14:12:52 +0900

    useSearchParams() 오류 해결

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,21 +1,32 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
-export function ProgressBarProvider({ children }: { children: React.ReactNode }) {
+function ProgressBarContent() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-
-  useEffect(() => {
-    NProgress.configure({ showSpinner: false }); // 스피너 숨기기
-  }, []);
 
   useEffect(() => {
     NProgress.done();
   }, [pathname, searchParams]);
 
-  return <>{children}</>;
+  return null;
+}
+
+export function ProgressBarProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    NProgress.configure({ showSpinner: false });
+  }, []);
+
+  return (
+    <>
+      <Suspense fallback={null}>
+        <ProgressBarContent />
+      </Suspense>
+      {children}
+    </>
+  );
 }
